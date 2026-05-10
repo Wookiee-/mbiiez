7b2cfd6927c185ce67dd5086ce57db342c5bf1f7
Wookiee- <taggart_james@hotmail.com>
2026-05-10 19:46:04 +1000

    Use same message style as RTV for maps/maplist/search output

--- plugins/plugin_rtvrtm.py
+++ plugins/plugin_rtvrtm.py
@@ -591,7 +591,7 @@ class plugin:
             all_maps.extend(list(secondary))
         
         if not all_maps:
-            self.instance.console.csay('^2[Maps] ^7No maps configured.')
+            self.instance.console.csay('^2[RTV] ^7No maps configured.')
             return
         
         all_maps.sort(key=lambda x: x.lower())
@@ -601,9 +601,9 @@ class plugin:
         total = len(all_maps)
         
         for idx, chunk in enumerate(chunks, 1):
-            self.instance.console.csay('^2[Maps ^7%i/%i^2] ^7%s' % (idx, len(chunks), ', '.join(chunk)))
+            self.instance.console.csay('^2[RTV] ^7%i/%i: %s' % (idx, len(chunks), ', '.join(chunk)))
         
-        self.instance.console.csay('^2[Maps] ^7Total: ^1%i ^7maps available for nomination' % total)
+        self.instance.console.csay('^2[RTV] ^7Total: ^1%i ^7maps available for nomination' % total)
 
     def handle_maplist(self, player_id, page):
         """Show map list with pagination"""
@@ -614,7 +614,7 @@ class plugin:
             all_maps.extend(list(secondary))
         
         if not all_maps:
-            self.instance.console.csay('^2[Voting] ^7Map voting is unavailable.')
+            self.instance.console.csay('^2[RTV] ^7Map voting is unavailable.')
             return
             
         current_map = self.current_map or ''
@@ -641,10 +641,9 @@ class plugin:
         page_maps = available_maps[start_idx:end_idx]
         
         if total_pages > 1:
-            self.instance.console.csay('^2[Maplist %i] ^7%s' % (page, ', '.join(page_maps)))
-            self.instance.console.csay('^2[Maplist] ^7Page %i of %i' % (page, total_pages))
+            self.instance.console.csay('^2[RTV] ^7Page %i/%i: %s' % (page, total_pages, ', '.join(page_maps)))
         else:
-            self.instance.console.csay('^2[Maplist] ^7%s' % ', '.join(page_maps))
+            self.instance.console.csay('^2[RTV] ^7%s' % ', '.join(page_maps))
 
     def handle_search(self, player_id, expression):
         """Search maps by expression"""
@@ -655,11 +654,11 @@ class plugin:
             all_maps.extend(list(secondary))
         
         if not all_maps:
-            self.instance.console.csay('^2[Voting] ^7Map voting is unavailable.')
+            self.instance.console.csay('^2[RTV] ^7Map voting is unavailable.')
             return
             
         if not expression:
-            self.instance.console.csay('^2[Search] ^7Usage: !search expression')
+            self.instance.console.csay('^2[RTV] ^7Usage: !search expression')
             return
         
         current_map = self.current_map or ''
@@ -672,7 +671,7 @@ class plugin:
         results = [m for m in available_maps if search_expr in m.lower()]
         
         if not results:
-            self.instance.console.csay('^2[Search] ^7No matches found for expression \'%s\'.' % expression)
+            self.instance.console.csay('^2[RTV] ^7No matches found for expression \'%s\'.' % expression)
             return
             
         results.sort(key=lambda x: x.lower())
@@ -680,9 +679,9 @@ class plugin:
         
         # Check if result is too long (similar to MAPLIST_MAX_SIZE)
         if len(result_str) > 750:
-            self.instance.console.csay('^2[Search] ^7Result for expression \'%s\' is too long.' % expression)
+            self.instance.console.csay('^2[RTV] ^7Result for expression \'%s\' is too long.' % expression)
         else:
-            self.instance.console.csay('^2[Search] ^7%s' % result_str)
+            self.instance.console.csay('^2[RTV] ^7%s' % result_str)
 
     def handle_vote_digit(self, player_id, player_name, vote_number):
         """Handle voting with digits during active voting"""
