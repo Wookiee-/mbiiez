1ebc2fec6902034932228f4d51ec2fad918a2040
Wookiee- <taggart_james@hotmail.com>
2026-05-10 17:06:37 +1000

    Remove threading Timer that was keeping server alive after stop

--- plugins/plugin_rtvrtm.py
+++ plugins/plugin_rtvrtm.py
@@ -11,13 +11,11 @@ Ported from the original rtvrtm.py to Python 3 and integrated as a mbiiEZ plugin
 
 import time
 import random
-import threading
 from datetime import datetime
 
 VERSION = '3.6c_py3'
 MAPLIST_MAX_SIZE = 750
 SLEEP_INTERVAL = 0.075
-MAP_STATUS_DELAY = 10.0  # Seconds to wait before broadcasting map status on load
 
 
 class plugin:
@@ -108,16 +106,6 @@ class plugin:
             self.instance.log_handler.log('[RTV/RTM] RTV is enabled - primary_maps: ' + str(len(self.maps)) + ', secondary_maps: ' + str(len(self.secondary_maps)))
         if self.rtm_enabled:
             self.instance.log_handler.log('[RTV/RTM] RTM is enabled')
-        
-        # Schedule map status broadcast after server is ready (avoid connection refused)
-        threading.Timer(MAP_STATUS_DELAY, self._broadcast_map_status).start()
-    
-    def _broadcast_map_status(self):
-        """Broadcast map loading status in-game after server is ready"""
-        try:
-            self.instance.say('^2[RTV/RTM] ^7Maps loaded: ^1' + str(len(self.maps)) + '^7 primary, ^1' + str(len(self.secondary_maps or [])) + '^7 secondary')
-        except Exception as e:
-            self.instance.log_handler.log('[RTV/RTM] Failed to broadcast map status: ' + str(e))
             
     def on_new_log_line(self, args):
         """Handle new log lines - for parsing RTV triggers from game log"""
